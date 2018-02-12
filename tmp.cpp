add1589431d2f73f17c1699049185c9eb64a887e
Amrit Dhar <adhar@uw.edu>
2018-02-11 18:09:40 -0800

    added phylo moments/derivatives main function

--- tmp.cpp
+++ tmp.cpp
@@ -25,6 +25,7 @@ class EdgeSet {
  public:
   EdgeSet(int label, Vector<int>&& elems)
       : label_(label), elems_(std::move(elems)) {
+    for (auto& elem : elems_) elem -= 1;
     std::sort(elems_.begin(), elems_.end());
   }
 
@@ -232,6 +233,11 @@ class NodeList {
 
 
 // helper functions
+
+int find_moment_derivative_id_index(const Vector<MomentDerivativeID>& ids,
+                                    const MomentDerivativeID& id) {
+  return std::lower_bound(ids.begin(), ids.end(), id) - ids.begin();
+}
 
 int find_list_id_index(const Vector<ListID>& ids,
                        const Vector<ListIDElement>& id_elems) {
@@ -898,4 +904,209 @@ int print_connected_moment_derivative_ids(VectorVector<int> esets_inp,
   }
 
   return md_ids.size();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////// PHYLO MOMENTS/DERIVATIVES ////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+//if (!tree.inherits("phylo")) Rcpp::stop("The tree object must be of class 'phylo'.");
+//if (tree.attr("order") != "cladewise") Rcpp::stop("The edge matrix must be in 'cladewise' order.");
+//if (max_order < 0) Rcpp::stop("'max_order' cannot be less than 0.");
+
+void update_node_lists(int child_node_ind, const arma::uvec& child_edge_inds,
+                       const Vector<EdgeList>& elists,
+                       Vector<NodeList>& nlists) {
+  for (std::size_t nlist_ind = 0; nlist_ind < nlists.size(); ++nlist_ind) {
+    for (const auto& ri_tup : nlists[nlist_ind].recursion_info()) {
+      // Extract the recursion information 3-tuple elements.
+      int left_elist_ind = std::get<0>(ri_tup);
+      int right_elist_ind = std::get<1>(ri_tup);
+      int choose_coef = std::get<2>(ri_tup);
+
+      // Update the current node list.
+      nlists[nlist_ind].elems().col(child_node_ind) +=
+          elists[left_elist_ind].elems().col(child_edge_inds(0)) %
+          elists[right_elist_ind].elems().col(child_edge_inds(1)) * choose_coef;
+    }
+  }
+}
+
+void update_edge_lists(
+    int edge_ind, int child_node_ind, const arma::cube& edge_mds,
+    Map<int, const PartitionSet&>::const_iterator edge_pset_it,
+    Map<int, const PartitionSet&>::const_iterator edge_psets_end_it,
+    const Vector<NodeList>& nlists, Vector<EdgeList>& elists) {
+  for (std::size_t elist_ind = 0; elist_ind < elists.size(); ++elist_ind) {
+    // Is the current edge in any of the partition sets?
+    if (edge_pset_it != edge_psets_end_it) {
+      // If so, is this partition set in the current edge list ID?
+      auto pset_inds_it =
+          elists[elist_ind].recursion_info_inds().find(edge_pset_it->second);
+      if (pset_inds_it != elists[elist_ind].recursion_info_inds().end()) {
+        // If so, we must account for the possible assignments of ID elements to
+        // the current edge.
+        int begin_ind, end_ind;
+        std::tie(begin_ind, end_ind) = pset_inds_it->second;
+
+        for (int ri_ind = begin_ind; ri_ind < end_ind; ++ri_ind) {
+          // Extract the recursion information 3-tuple elements.
+          const auto& ri_tup = elists[elist_ind].recursion_info()[ri_ind];
+          const ListIDElement& curr_id_elem = std::get<0>(ri_tup);
+          int nlist_ind = std::get<1>(ri_tup);
+          int choose_coef = std::get<2>(ri_tup);
+
+          // Update the current edge list.
+          elists[elist_ind].elems().col(edge_ind) +=
+              edge_mds.slice(curr_id_elem.order()) *
+              nlists[nlist_ind].elems().col(child_node_ind) * choose_coef;
+        }
+      }
+    }
+
+    // Update the current edge list.
+    // (Note: this update accounts for the case where no ID element is assigned
+    // to the current edge.)
+    elists[elist_ind].elems().col(edge_ind) +=
+        edge_mds.slice(0) * nlists[elist_ind].elems().col(child_node_ind);
+  }
+}
+
+Map<std::string, double> phylo_moments_derivatives(
+    const Rcpp::List& tree, const arma::mat& Q, const arma::mat& B,
+    const arma::vec& pi, VectorVector<int> esets_inp, int max_order, Mode mode,
+    const arma::ivec& tip_data) {
+  // Extract the necessary elements from the tree object.
+  arma::imat edge = tree["edge"];
+  const Vector<std::string>& tip_labels = tree["tip.label"];
+  const arma::vec& edge_lengths = tree["edge.length"];
+  int num_int_nodes = tree["Nnode"];
+
+  edge -= 1;
+  int num_edges = edge.n_rows;
+  int num_term_nodes = tip_labels.size();
+  int root_node_ind = num_term_nodes;
+
+  // Initialize the counting table.
+  arma::mat choose = choose_table(max_order);
+
+  // Construct the edge sets and partition sets.
+  Vector<EdgeSet> esets = create_edge_sets(esets_inp);
+  Vector<PartitionSet> psets = partition_edge_sets(esets);
+
+  // Form the (edge index, partition set) inverse map.
+  Map<int, const PartitionSet&> edge_psets = create_edge_pset_map(psets);
+
+  // Generate the moment/derivative IDs and list IDs.
+  Vector<MomentDerivativeID> md_ids =
+      get_moment_derivative_ids(esets, max_order);
+  Vector<ListID> list_ids = get_list_ids(psets, max_order);
+
+  // Create the node lists and edge lists.
+  Vector<NodeList> nlists;
+  nlists.reserve(list_ids.size());
+  Vector<EdgeList> elists;
+  elists.reserve(list_ids.size());
+
+  for (const auto& list_id : list_ids) {
+    nlists.emplace_back(list_id, list_ids, choose, Q.n_rows,
+                        num_int_nodes + num_term_nodes);
+    elists.emplace_back(list_id, list_ids, choose, Q.n_rows, num_edges);
+  }
+
+  // Perform the postorder recursion.
+  // (Note: we iterate up the edge matrix because, by default, it is arranged in
+  // a preorder format.)
+  for (int edge_ind = num_edges - 1; edge_ind >= 0; --edge_ind) {
+    int child_node_ind = edge(edge_ind, 1);
+
+    ////
+    //// First, we consider the node lists at the current child node.
+    ////
+
+    // Is the current child node an internal node?
+    if (child_node_ind >= root_node_ind) {
+      // If so, update each node list using the appropriate edge lists at the
+      // two branches below the current child node.
+      arma::uvec child_edge_inds = arma::find(edge.col(0) == child_node_ind);
+      update_node_lists(child_node_ind, child_edge_inds, elists, nlists);
+    } else {
+      // Otherwise, initialize the "empty" (i.e. partial likelihood) node list
+      // at the given terminal node.
+      // (Note: the "empty" node list is always the first element in the node
+      // list vector.)
+
+      // Do we have observed data at the terminal nodes?
+      if (tip_data.n_elem > 0) {
+        nlists[0].elems()(tip_data(child_node_ind), child_node_ind) = 1;
+      } else {
+        nlists[0].elems().col(child_node_ind).fill(1);
+      }
+    }
+
+    ////
+    //// Second, we consider the edge lists at the current edge.
+    ////
+
+    // Is the current edge in any of the partition sets?
+    auto edge_pset_it = edge_psets.find(edge_ind);
+
+    // If so, compute the necessary CTMC moments/derivatives at the current
+    // edge.
+    // Otherwise, compute only the zeroth CTMC moment/derivative (i.e. the
+    // transition probability matrix) at the current edge.
+    int edge_max_order = (edge_pset_it != edge_psets.end()) ? max_order : 0;
+    arma::cube edge_mds = ctmc_moments_derivatives(edge_lengths(edge_ind), Q, B,
+                                                   edge_max_order, mode);
+
+    // Update the edge lists at the current edge.
+    update_edge_lists(edge_ind, child_node_ind, edge_mds, edge_pset_it,
+                      edge_psets.end(), nlists, elists);
+  }
+
+  // Update the node lists at the root node.
+  arma::uvec root_edge_inds = arma::find(edge.col(0) == root_node_ind);
+  update_node_lists(root_node_ind, root_edge_inds, elists, nlists);
+
+  // Connect the list IDs to the moment/derivative IDs and calculate the
+  // moments/derivatives by accumulating dot products between the root
+  // distribution and the node lists at the root node.
+  arma::vec tree_mds(md_ids.size(), arma::fill::zeros);
+  for (const auto& nlist : nlists) {
+    Vector<MomentDerivativeID> conn_md_ids =
+        find_connected_moment_derivative_ids(nlist.id(), esets);
+
+    for (const auto& conn_md_id : conn_md_ids) {
+      int conn_md_id_ind = find_moment_derivative_id_index(md_ids, conn_md_id);
+      tree_mds(conn_md_id_ind) +=
+          arma::dot(pi, nlist.elems().col(root_node_ind));
+    }
+  }
+
+  // Prepare the output map.
+  Map<std::string, double> tree_mds_map;
+  for (std::size_t i = 0; i < md_ids.size(); ++i) {
+    // Create the moment/derivative ID label.
+    std::string md_id_label;
+    for (const auto& id_elem : md_ids[i]) {
+      md_id_label.append(id_elem.order(),
+                         std::to_string(id_elem.set().label() + 1)[0]);
+    }
+
+    // Cache the moment/derivative.
+    tree_mds_map.emplace(std::move(md_id_label), tree_mds(i));
+  }
+
+  return tree_mds_map;
+}
+
+// [[Rcpp::export]]
+Map<std::string, double> phylomd_wrap(const Rcpp::List& tree,
+                                      const arma::mat& Q, const arma::mat& B,
+                                      const arma::vec& pi,
+                                      VectorVector<int> esets_inp,
+                                      int max_order,
+                                      const arma::ivec& tip_data) {
+  return phylo_moments_derivatives(tree, Q, B, pi, esets_inp, max_order,
+                                   Mode::MOMENTS, tip_data);
 }
